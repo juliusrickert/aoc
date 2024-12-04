b33e6113b0cc20a3b3f51133e36e990f9fe3c2c8
Julius Rickert <git@juliusrickert.de>
2024-12-04 22:53:52 +0100

    feat(2024/day3): add part two

--- 2024/day3/main.exs
+++ 2024/day3/main.exs
@@ -9,6 +9,18 @@ defmodule Day3 do
       |> Enum.sum()
 
     IO.puts("part1: #{result}")
+
+    no_donts =
+      String.split(input, "do()")
+      |> Enum.map(fn do_first -> String.split(do_first, "don't()", parts: 2) |> hd() end)
+      |> Enum.join()
+
+    result_without_donts =
+      Regex.scan(regex, no_donts)
+      |> Enum.map(fn [_, mul1, mul2] -> String.to_integer(mul1) * String.to_integer(mul2) end)
+      |> Enum.sum()
+
+    IO.puts("part2: #{result_without_donts}")
   end
 end
 
