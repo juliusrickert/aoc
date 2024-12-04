3885853eb4cb5e5114d4525511b5118e281a33bd
Julius Rickert <git@juliusrickert.de>
2024-12-04 22:33:09 +0100

    feat(2024/day1): add part one

---
+++ 2024/day1/main.exs
@@ -0,0 +1,32 @@
+defmodule Day1 do
+  def run do
+    # Creates an array of arrays
+    # Each line is an array that contains an array of the two values on the line.
+    input =
+      IO.read(:stdio, :eof)
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> String.split(line) end)
+
+    left =
+      input
+      |> Enum.map(fn [left, _] -> String.to_integer(left) end)
+      |> Enum.sort()
+
+    right =
+      input
+      |> Enum.map(fn [_, right] -> String.to_integer(right) end)
+      |> Enum.sort()
+
+    values = Enum.zip(left, right)
+
+    # part1
+    total_distance =
+      values
+      |> Enum.map(fn {left, right} -> abs(left - right) end)
+      |> Enum.sum()
+
+    IO.puts("part1: #{total_distance}")
+  end
+end
+
+Day1.run()
