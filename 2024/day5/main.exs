b14e3919b8331cae7d5502e557f23d79d8453c4f
Julius Rickert <git@juliusrickert.de>
2024-12-08 14:46:54 +0100

    feat(2024/day5): add part one

---
+++ 2024/day5/main.exs
@@ -0,0 +1,69 @@
+defmodule Day5 do
+  def run do
+    input =
+      IO.read(:stdio, :eof)
+
+    [rules, instances] =
+      input
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(fn block ->
+        block
+        |> String.split("\n", trim: true)
+      end)
+
+    rules =
+      rules
+      |> Enum.map(fn rule ->
+        rule
+        |> String.split("|", trim: true)
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
+      end)
+
+    instances =
+      instances
+      |> Enum.map(fn rule ->
+        rule
+        |> String.split(",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    instances
+    |> Enum.map(fn instance ->
+      filtered_rules = filter_rules(instance, rules)
+      (correct_order?(Enum.reverse(instance), filtered_rules) && middle_number(instance)) || 0
+    end)
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+
+  # Filters the rules for a given instance.
+  # Both the antecedent and the consequent must be part of the instance.
+  defp filter_rules(instance, rules) do
+    rules
+    |> Enum.filter(fn {antecedent, consequent} ->
+      Enum.member?(instance, antecedent) and Enum.member?(instance, consequent)
+    end)
+  end
+
+  # is_ancedent is true if the page is on the "left side" of our rules.
+  # As we have filtered the rules, they only include rules applicable to the given instance.
+  # Consequently, the last page of any instance must not be the antecedent.
+  defp is_antecedent?(page, rules) do
+    rules
+    |> Enum.any?(fn {antecedent, _} -> antecedent == page end)
+  end
+
+  # correct_order? checks whether a given reversed instance adheres to the rules.
+  defp correct_order?([page | reversed_instance], rules) do
+    not is_antecedent?(page, rules) and
+      correct_order?(reversed_instance, filter_rules(reversed_instance, rules))
+  end
+
+  defp correct_order?([], _), do: true
+
+  defp middle_number([num]), do: num
+  defp middle_number([_ | rest]), do: middle_number(Enum.reverse(rest))
+end
+
+Day5.run()
