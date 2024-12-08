caf935d6908b5199f41be2ef937c5b1266a5152c
Julius Rickert <git@juliusrickert.de>
2024-12-08 21:21:46 +0100

    WIP: day6 part2

--- 2024/day6/main.exs
+++ 2024/day6/main.exs
@@ -19,42 +19,62 @@ defmodule Day6 do
 
     # Find starting point.
     sp = starting_point(matrix1)
-    matrix1 = set_element(sp, matrix1, ?X)
 
-    # Orient matrix.
-    {sp, rotated_matrix} = rotate_90deg_anticlockwise(sp, matrix1)
-    {sp, rotated_matrix} = rotate_90deg_anticlockwise(sp, rotated_matrix)
-    {sp, rotated_matrix} = rotate_90deg_anticlockwise(sp, rotated_matrix)
+    # Add orientation marker
+    matrix1 = set_element({0, 0}, matrix1, ?0)
 
-    step(sp, rotated_matrix)
-    |> List.flatten()
-    |> Enum.frequencies()
-    |> Map.fetch!(?X)
+    part1 =
+      orient_and_step(sp, matrix1)
+      |> orient_matrix()
+      |> find_all(?X)
+
+    part1
+    |> length()
     |> IO.inspect(label: "part1")
+
+    part1
+    |> Enum.map(&IO.inspect/1)
+
+    # |> Enum.map(fn coords ->
+    #   orient_and_step(sp, set_element(coords, matrix1, ?#))
+    # end)
+    # |> Enum.filter(fn item -> item == :loop end)
+    # |> length()
+    # |> IO.inspect(label: "part2")
+
+    # 719 is too low
   end
 
   defp starting_point(matrix) do
-    matrix
-    |> Enum.with_index(fn line, row ->
-      line
-      |> Enum.with_index(fn element, col -> {row, col, element} end)
-    end)
-    |> List.flatten()
-    |> Enum.filter(fn {_, _, element} -> element == ?^ end)
-    |> Enum.map(fn {row, col, _} -> {row, col} end)
-    |> hd()
+    matrix |> find_all(?^) |> hd()
+  end
+
+  defp orient_and_step(sp, matrix) do
+    # Orient matrix.
+    {sp, matrix} = rotate_90deg_anticlockwise(sp, matrix)
+    {sp, matrix} = rotate_90deg_anticlockwise(sp, matrix)
+    {sp, matrix} = rotate_90deg_anticlockwise(sp, matrix)
+    step(sp, matrix)
   end
 
   defp step({row, col}, matrix) do
-    element = get_element({row, col + 1}, matrix)
+    element = get_element({row, col}, matrix)
+    next_element = get_element({row, col + 1}, matrix)
 
-    if element == ?# do
-      {rotated_coordinates, rotated_matrix} = rotate_90deg_anticlockwise({row, col}, matrix)
-      step(rotated_coordinates, rotated_matrix)
+    if next_element == ?# do
+      if element == ?X do
+        # We are in a loop.
+        IO.puts("loop found")
+        :loop
+      else
+        {rotated_coordinates, rotated_matrix} = rotate_90deg_anticlockwise({row, col}, matrix)
+        step(rotated_coordinates, rotated_matrix)
+      end
     else
       matrix = set_element({row, col}, matrix, ?X)
 
-      if element == ?* do
+      if next_element == ?* do
+        IO.puts("solution found")
         matrix
       else
         step({row, col + 1}, matrix)
@@ -93,6 +113,27 @@ defmodule Day6 do
   defp mirror(matrix) do
     matrix
     |> Enum.map(&Enum.reverse/1)
+  end
+
+  # find_all returns the coordinates of all occurences of value in the matrix.
+  defp find_all(matrix, value) do
+    matrix
+    |> Enum.with_index(fn line, row ->
+      line
+      |> Enum.with_index(fn element, col -> {row, col, element} end)
+    end)
+    |> List.flatten()
+    |> Enum.filter(fn {_, _, element} -> element == value end)
+    |> Enum.map(fn {row, col, _} -> {row, col} end)
+  end
+
+  defp orient_matrix(matrix) do
+    if get_element({0, 0}, matrix) == ?0 do
+      matrix
+    else
+      {_, matrix} = rotate_90deg_anticlockwise({0, 0}, matrix)
+      matrix
+    end
   end
 end
 
