c56fd2f8eb567088351337359105f2967178ab21
Julius Rickert <git@juliusrickert.de>
2024-12-12 00:16:08 +0100

    feat(2024/day8): add part two

--- 2024/day8/main.exs
+++ 2024/day8/main.exs
@@ -18,6 +18,20 @@ defmodule Day8 do
     |> MapSet.new()
     |> MapSet.size()
     |> IO.inspect(label: "part1")
+
+    input
+    |> List.flatten()
+    |> Enum.frequencies()
+    |> Map.delete(?.)
+    |> Map.keys()
+    |> Enum.map(fn frequency ->
+      locations(frequency, input)
+      |> locations_to_antinodes(50, 50, true)
+    end)
+    |> List.flatten()
+    |> MapSet.new()
+    |> MapSet.size()
+    |> IO.inspect(label: "part2")
   end
 
   # locations returns the coordinates of all occurrences of the subject in the matrix.
@@ -31,23 +45,47 @@ defmodule Day8 do
     |> Enum.map(fn {row, col, _} -> {row, col} end)
   end
 
-  defp locations_to_antinodes(locations, rows, cols) do
+  defp locations_to_antinodes(locations, rows, cols, part2 \\ false) do
     pairings = for a <- locations, b <- locations, a < b, do: {a, b}
 
     antinode_candidates =
-      Enum.map(pairings, fn {{row0, col0}, {row1, col1}} ->
-        diff_row = row0 - row1
-        diff_col = col0 - col1
-        [{row0 + diff_row, col0 + diff_col}, {row1 - diff_row, col1 - diff_col}]
-      end)
-      |> List.flatten()
+      unless part2 do
+        Enum.map(pairings, fn {{row0, col0}, {row1, col1}} ->
+          diff_row = row0 - row1
+          diff_col = col0 - col1
+          [{row0 + diff_row, col0 + diff_col}, {row1 - diff_row, col1 - diff_col}]
+        end)
+        |> List.flatten()
+      else
+        in_line =
+          Enum.map(pairings, fn {{row0, col0}, {row1, col1}} ->
+            diff_row = row0 - row1
+            diff_col = col0 - col1
+
+            gcd = Integer.gcd(diff_row, diff_col)
+            diff_row = div(diff_row, gcd)
+            diff_col = div(diff_col, gcd)
+
+            list =
+              for i <- 1..max(rows, cols),
+                  do: [
+                    {row0 + i * diff_row, col0 + i * diff_col},
+                    {row1 - i * diff_row, col1 - i * diff_col}
+                  ]
+
+            List.flatten(list)
+          end)
+          |> List.flatten()
+
+        in_line ++ locations
+      end
 
     # Filter out-of-bound & antenna locations
     Enum.filter(antinode_candidates, fn {row, col} ->
       row >= 0 && col >= 0 && row < rows && col < cols
     end)
     |> Enum.filter(fn element ->
-      not Enum.member?(locations, element)
+      part2 or not Enum.member?(locations, element)
     end)
   end
 end
